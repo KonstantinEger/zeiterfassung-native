43e98a06d44f7028a157a50b507baa1e92264b7f
sxphie.1002 <sophieweinhold1002@gmail.com>
2023-12-11 14:07:25 +0100

    Request View Page

---
+++ pages/RequestView.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export function RequestViewPage(){
+    return (
+        <View>
+            <Text>Use this page for time overview.</Text>
+        </View>
+    )
+}
